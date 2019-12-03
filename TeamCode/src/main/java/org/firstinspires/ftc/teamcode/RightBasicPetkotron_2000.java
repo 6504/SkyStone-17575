70e340bdc7d82b3a264c86ff2beee43523e9122e
xeu100 <liamryde@gmail.com>
2019-12-03 16:15:38 -0500

    yoink v1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RightBasicPetkotron_2000.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RightBasicPetkotron_2000.java
@@ -6,9 +6,9 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name="PetkoTron_2000: AutonomousOp", group="PetkoTron_2000")
+@Autonomous(name="PetkoTron_2000: RIGHT BASIC", group="PetkoTron_2000")
 //@Disabled
-public class AutonomousOpPetkotron_2000 extends LinearOpMode {
+public class RightBasicPetkotron_2000 extends LinearOpMode {
 
     //Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
@@ -16,7 +16,7 @@ public class AutonomousOpPetkotron_2000 extends LinearOpMode {
 
     static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
     static final double     DRIVE_GEAR_REDUCTION    = 4.0 ;     // This is < 1.0 if geared UP
-    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     WHEEL_DIAMETER_INCHES   = 3.85826772 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
 
     static final double     DRIVE_SPEED             = 0.6;
@@ -27,7 +27,6 @@ public class AutonomousOpPetkotron_2000 extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
         while (opModeIsActive()) {
-
 
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.update();
